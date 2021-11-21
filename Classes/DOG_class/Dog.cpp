56773c49a8c20e796da9b501da78cd0d1937c14f
jj5600 <57789684+jj5600@users.noreply.github.com>
2021-11-21 12:26:26 -0800

    Update Dog.cpp

--- Classes/DOG_class/Dog.cpp
+++ Classes/DOG_class/Dog.cpp
@@ -4,14 +4,35 @@
 #include<string>
 #include<cstring>
 using namespace std;
+//this is called the constructor
+//and it is usually used to initialize values
 
+Dog::Dog()
+{
+	cout<<"welcome to Dog class\n";
+	weight=0.0;
+	favperson="n/a";
+	favtreat="n/a";
+}
 void Dog::setWeight(double w)
 {
-	weight=w;
+	if(w>0)
+	{
+		weight=w;
+	}
+	else 
+	{
+		exit(EXIT_FAILURE);
+	}
 }
 void Dog::setfavPerson(string p)
 {
 	favperson=p;
+
+}
+void Dog::setfavTreat(string t)
+{
+	favtreat=t;
 
 }
 double Dog::getWeight() const
