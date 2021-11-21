c7d63907be2b4956584b6063dad343b9c436feb3
jj5600 <57789684+jj5600@users.noreply.github.com>
2021-11-21 12:26:47 -0800

    Update Dog.h

--- Classes/DOG_class/Dog.h
+++ Classes/DOG_class/Dog.h
@@ -7,13 +7,21 @@ class Dog
 	private:
 	double weight;
 	string favperson;
+	string favtreat;
 	public:
-
+	Dog();
 	void setWeight(double w);
 	void setfavPerson(string p);
+	void setfavTreat(string t);
 
 	double getWeight() const;
 	string getfavPerson() const;
+
+	//this is callled an inline funtion defintion 
+	string getfavTreat() const
+	{
+		return favtreat;
+	}
 	
 
 };
