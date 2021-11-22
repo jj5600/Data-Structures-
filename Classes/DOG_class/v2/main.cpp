3f4be3df9f5b4f16472a5dd142f50404c969f8a6
jj5600 <57789684+jj5600@users.noreply.github.com>
2021-11-22 12:24:23 -0800

    Create main.cpp

---
+++ Classes/DOG_class/v2/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<string>
+#include<cstring>
+#include "Dog.h"
+using namespace std;
+int main() {	
+
+	cout << "Hello World!\n";
+	string a;
+	string b;
+
+	Dog Frankie(0);
+	Dog Eddie(0);
+	//dynamically allocating another dog
+	//welcome to dog clsss printed twice
+	Dog *ptrN=nullptr;
+	ptrN=new Dog(0);
+
+	Dog *ptrE=nullptr;
+	Dog *ptrF=nullptr;
+	ptrF=&Frankie;
+	ptrE=&Eddie;
+	ptrF->setfavPerson("Joseph");
+	ptrE->setfavPerson("Katie");
+	ptrF->setWeight(98.2);
+	ptrE->setWeight(99.3);
+	ptrF->setfavTreat("Mint Bone");
+	ptrE->setfavTreat("Beef Stick");
+
+	cout<<"Frankie's Favorite person is : "<<ptrF->getfavPerson()<<endl;
+	cout<<"Eddies' Favorite person is : "<<ptrE->getfavPerson()<<endl;
+
+	cout<<"Frankies Weight is : "<<ptrF->getWeight()<<endl;
+	cout<<"Eddies Weight is: "<<ptrE->getWeight()<<endl;
+	
+	cout<<"Frankies favorite treat is : "<<ptrF->getfavTreat()<<endl;
+	cout<<"Eddies favorite treat is: "<<ptrE->getfavTreat()<<endl;
+
+	delete ptrN;
+	ptrN=nullptr;
+	return 0;
+} 
