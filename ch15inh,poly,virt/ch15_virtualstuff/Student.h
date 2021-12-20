c55c950f9a4ce0b24bd4534740cd2bdf5f7baa74
jj5600 <57789684+jj5600@users.noreply.github.com>
2021-12-20 15:40:31 -0800

    Create Student.h

---
+++ ch15inh,poly,virt/ch15_virtualstuff/Student.h
@@ -0,0 +1,44 @@
+#ifndef STUDNET_H
+#define STUDNET_H
+#include<string>
+#include<iostream>
+using namespace std;
+class Student
+{
+	protected:
+	string name;
+	string idNumber;
+	int yearAdmitted;
+	public:
+		Student()
+		{
+			name="";
+			idNumber="";
+			yearAdmitted=0;
+		
+		}
+		Student(string n, string id, int year)
+		{
+			set(n,id,year);
+		}
+		void set(string n, string id, int year)
+		{	name=n;
+			idNumber=id;
+			yearAdmitted=year;
+		}
+		const string getName() const
+		{
+				return name;
+		}
+		const string getIdNum() const
+		{
+			return idNumber;
+		}
+		int getYearAdmitted() const
+		{
+			return yearAdmitted;
+		}
+		virtual int getRemainingHours() const=0;
+		
+};
+#endif
